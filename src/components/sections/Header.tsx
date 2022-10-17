7f61e75a86b0e2411185fea7bf9d4bc1a28cc94a
derodero24 <k.itsh20524abc@gmail.com>
2022-10-17 09:12:35 +0900

    update

--- src/components/sections/Header.tsx
+++ src/components/sections/Header.tsx
@@ -1,8 +1,6 @@
 import Link from 'next/link';
 
 import GitHubButton from '../elements/GitHubButton';
-import LangButton from '../elements/LangButton';
-import ThemeButton from '../elements/ThemeButton';
 
 export default function Header() {
   return (
@@ -11,8 +9,6 @@ export default function Header() {
         <a className="text-xl">Next.js</a>
       </Link>
       <nav className="flex items-center space-x-6">
-        <ThemeButton className="header-btn" />
-        <LangButton className="header-btn" />
         <GitHubButton className="header-btn" />
       </nav>
     </header>
