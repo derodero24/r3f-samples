81cc8a3b07536c20c9e8f3c23cf3f6d626233d7f
derodero24 <k.itsh20524abc@gmail.com>
2022-10-19 13:36:29 +0900

    update shader /points-animation

--- src/components/points-animation/shader.ts
+++ src/components/points-animation/shader.ts
@@ -39,9 +39,11 @@ export const fragmentShader = `
 precision mediump float; // 精度修飾子
 
 void main() {
-  // 点の中心からの距離を元に透明度を指定し、丸くする
-  float alpha = 1. - smoothstep(0.4995, 0.5005, length(gl_PointCoord - vec2(0.5)));
+  // ■ → ●
+  float dist = length(gl_PointCoord - vec2(0.5)); // 点の中心からの距離
+  if (dist > 0.5 ) discard;
 
-  gl_FragColor = vec4(vec3(0.5), alpha);
+  // 色セット
+  gl_FragColor = vec4(vec3(0.5), 1.);
 }
 `;
