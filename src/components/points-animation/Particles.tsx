993a82309119b23021658f4fc1063b10f4349327
derodero24 <k.itsh20524abc@gmail.com>
2022-10-20 16:13:18 +0900

    update sphere points

--- src/components/points-animation/Particles.tsx
+++ src/components/points-animation/Particles.tsx
@@ -5,7 +5,6 @@ import {
   BufferAttribute,
   BufferGeometry,
   Mesh,
-  SphereGeometry,
   TorusGeometry,
   TorusKnotGeometry,
 } from 'three';
@@ -13,16 +12,27 @@ import {
 import { normalRand, randomChoise, to2dArray } from '../../utils';
 import { fragmentShader, vertexShader } from './shader';
 
-export function geometryPositions(
+function geometryPositions(
   geometry: BufferGeometry,
   n_points: number,
 ): Float32Array {
   const positions = geometry.attributes['position']!.array;
   console.log(positions.length);
-
   const reshaped = to2dArray(Array.from(positions), 3);
   const chosed = randomChoise(reshaped, n_points);
   return Float32Array.from(chosed.flat());
+}
+
+function randomOnSphere(r: number) {
+  // 球面上の一様乱数
+  const cosTheta = -2.0 * Math.random() + 1.0;
+  const sinTheta = Math.sqrt(1.0 - cosTheta * cosTheta);
+  const phi = 2.0 * Math.PI * Math.random();
+  return [
+    r * sinTheta * Math.cos(phi),
+    r * sinTheta * Math.sin(phi),
+    r * cosTheta,
+  ];
 }
 
 const N_POINTS = 5000;
@@ -36,7 +46,9 @@ export default function Particles() {
 
   const points_data = useMemo(
     () => ({
-      sphere: geometryPositions(new SphereGeometry(2, 80, 80), N_POINTS),
+      sphere: Array.from({ length: N_POINTS })
+        .map(() => randomOnSphere(2.5))
+        .flat(),
       torus: geometryPositions(new TorusGeometry(2, 0.8, 50, 140), N_POINTS),
       torusknot: geometryPositions(
         new TorusKnotGeometry(2, 0.3, 280, 22),
@@ -104,7 +116,7 @@ export default function Particles() {
 
   useEffect(() => {
     // 8秒毎にオブジェクト変更
-    changeObject();
+    // changeObject();
     const timer = setInterval(changeObject, 8_000);
     return () => clearInterval(timer);
   }, []);
