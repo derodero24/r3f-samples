e9bc10e05554671785cd274257eb201fed5ac686
derodero24 <k.itsh20524abc@gmail.com>
2023-11-23 13:14:33 +0900

    update

--- src/pages/samples/endless-road.tsx
+++ src/pages/samples/endless-road.tsx
@@ -7,15 +7,15 @@ import type { NextPage } from 'next';
 
 const Page: NextPage = () => {
   return (
-    <div className="bg-gray-800 h-screen flex items-end">
-      <div className="w-full h-1/2">
+    <div className="flex h-screen items-end bg-gray-800">
+      <div className="h-1/2 w-full">
         <Canvas>
           {/* <OrbitControls /> */}
           <Road lineColor="#9c88ff" lineWidth={0.002} />
           <Debug />
         </Canvas>
       </div>
-      <div className="absolute w-full h-1/2 from-gray-800 to-transparent bg-gradient-to-b" />
+      <div className="absolute h-1/2 w-full bg-gradient-to-b from-gray-800 to-transparent" />
     </div>
   );
 };
